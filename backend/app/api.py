d36afc845ef48c6e5a846fed8e08821b9d4e7dc2
Oliver Burke <oliver@predictx.com>
2022-08-05 09:18:20 +0100

    add initial setup of react frontend
    add cors middleware to api

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from fastapi import FastAPI, HTTPException, Path
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 description = """
@@ -30,6 +31,17 @@ app = FastAPI(
         "email": "hi@oliverburke.uk",
     },
     openapi_tags=tags,
+)
+
+
+origins = ["http://localhost:3000", "localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import {
+    Box,
+    Button,
+    Flex,
+    Input,
+    InputGroup,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Stack,
+    Text,
+    useDisclosure
+} from "@chakra-ui/react";
+
+const TodosContext = React.createContext({
+  todos: [], fetchTodos: () => {}
+})
